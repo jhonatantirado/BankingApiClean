4098ad3f60fe34d9d6a8b2402a5a6cafcae23d4d
jgsistem <jgsistem@hotmail.com>
2018-06-30 15:49:26 -0500

    crud Customer

--- src/main/resources/db/migration/V1_1__create_customer.sql
+++ src/main/resources/db/migration/V1_1__create_customer.sql
@@ -2,11 +2,11 @@ CREATE TABLE customer (
   customer_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
-  birthDate DATE NULL,
+  birthDate DATE null,
   documentNumber VARCHAR(50) NOT NULL,
   isactive BOOLEAN,
-  cellphone VARCHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL,
+  cellphone VARCHAR(50) NULL,
+  email VARCHAR(50) NULL,
   PRIMARY KEY (customer_id),
   INDEX IX_customer_last_first_name (last_name, first_name)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
